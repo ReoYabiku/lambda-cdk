90a5dcdf10435a108b9babd096036beaae3b7448
ReoYabiku <alcremikyu.z@gmail.com>
2024-12-06 23:58:34 +0900

    deploy lambda

--- lib/lambda-cdk-stack.ts
+++ lib/lambda-cdk-stack.ts
@@ -1,16 +1,44 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LambdaCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //HelloWorldするLambdaの作成
+    const helloFunc = new lambda.Function(this, "HelloLambda", {
+      runtime: lambda.Runtime.PYTHON_3_12,
+      handler: "hello.lambda_handler",
+      code: lambda.Code.fromAsset('lambda', {
+        bundling: {
+          image: lambda.Runtime.PYTHON_3_12.bundlingImage,
+          command: [
+            'bash',
+            '-c',
+            'cp -au . /asset-output'
+          ],
+        }
+      })
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // API Gateway RestAPIの作成
+    const nameRestApi ="Rest API with Lambda auth";
+    const restApi = new apigateway.RestApi(this, nameRestApi, {
+      restApiName: `Rest_API_with_Lambda_auth`,
+      deployOptions: {
+        stageName: 'v1',
+      },
+    });
+
+    //API Gatewayにリクエスト先のリソースを追加
+    const restApiHelloWorld = restApi.root.addResource('hello_world');
+
+    //リソースにGETメソッド、Lambda統合プロキシを指定
+    restApiHelloWorld.addMethod(
+      'GET',
+      new apigateway.LambdaIntegration(helloFunc)
+    );
   }
 }
